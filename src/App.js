f137303dfdf916464c5f76849ff7ec08d82b01e7
Osama Azhar <osamaazhar.edu@gmail.com>
2021-03-27 15:43:29 +0500

    Boiler Plate Ready. Components Ready For Code.

--- src/App.js
+++ src/App.js
@@ -1,23 +1,23 @@
 import logo from './logo.svg';
 import './App.css';
+import Card from './Components/Card/Card';
+import Carousel from './Components/Carousel/Carousel';
+
+import {Switch,Route,BrowserRouter} from "react-router-dom"
+import LandingPage from './Pages/LandingPage/LandingPage';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BrowserRouter>
+      
+      <Switch>
+        <Route path="/" component={LandingPage} exact />
+      </Switch>
+      
+      
+      
+      </BrowserRouter>
     </div>
   );
 }
